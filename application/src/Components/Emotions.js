caad42b3363c84ce221b408982966483997e69c5
Marta Budnik <mbudnikm97@gmail.com>
2019-04-14 20:07:51 +0200

    change emotions color

--- application/src/Components/Emotions.js
+++ application/src/Components/Emotions.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Emotions = () => {
     return (
         <div>
-            <h1 style={{color: '#B53471'}}>Emocje</h1>
+            <h1 style={{color: '#5f27cd'}}>Emocje</h1>
         </div>
     )
 }
