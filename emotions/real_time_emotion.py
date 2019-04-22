503d8eddf5e75ceb51b2e8e9891d5b34ed80f213
Carlloy <karolinazdon@gmail.com>
2019-04-22 23:24:00 +0200

    real time video emotion detection implemented

---
+++ emotions/real_time_emotion.py
@@ -0,0 +1,60 @@
+from keras.preprocessing.image import img_to_array
+import cv2
+from keras.models import load_model
+import numpy as np
+
+# model paths
+haar_cascade_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+# emotion_model_path = 'models/_mini_XCEPTION.102-0.66.hdf5'
+emotion_model_path = 'models/_simpler_CNN.10-0.54.hdf5'
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# loading models
+face_detection = cv2.CascadeClassifier(haar_cascade_path)
+# without compile=False code won't compile - Tensorflow
+emotion_model = load_model(emotion_model_path, compile=False)
+
+# labels of  emotion in polish language because of target user
+emotions_labels = ['Zlosc', 'Znieskaczenie', 'Strach', 'Radosc', 'Smutek', 'Zaskoczenie', 'Obojetnosc']
+
+# starting video streaming
+cv2.namedWindow('Emocje')
+camera = cv2.VideoCapture(0)
+while True:
+	frame = camera.read()[1]
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# detect face
+	faces = face_detection.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(100, 100),
+											flags=cv2.CASCADE_SCALE_IMAGE)
+
+	# When face is detected
+	if len(faces) > 0:
+		for (x, y, w, h) in faces:
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 205), 2)
+			face = gray[y:y + h, x:x + w]
+			# resize the image to be passed to the neural network
+			resized_face = cv2.resize(face, (48, 48))
+			resized_face = resized_face.astype("float32") / 255.0
+			resized_face = img_to_array(resized_face)
+			resized_face = np.expand_dims(resized_face, axis=0)
+
+			predictions = emotion_model.predict(resized_face)
+			emotion_probability = np.max(predictions)
+			# the dominant emotion is the one with the highest probability
+			label = emotions_labels[predictions.argmax()]
+	else:
+		continue
+
+	for index, emotion in enumerate(emotions_labels):
+		# construct the label text
+		emotion_result = "{}: {:.2f}%".format(emotion, emotion_probability * 100)
+		cv2.putText(frame, emotion_result, (10, index * 20 + 20), font, 0.75, (0, 0, 205), 2)
+		cv2.putText(frame, label, (x, y - 10), font, 1, (0, 0, 205), 2)
+
+	cv2.imshow('Emocje', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+camera.release()
+cv2.destroyAllWindows()
